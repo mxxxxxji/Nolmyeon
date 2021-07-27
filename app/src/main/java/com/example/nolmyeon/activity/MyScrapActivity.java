73f5c6e1ca5a3f0c71b7a577090a49c654ae4c93
10015206 <mxxxxxji>
2021-07-27 17:59:06 +0900

    스크랩액티비티 수정중

--- app/src/main/java/com/example/nolmyeon/activity/MyScrapActivity.java
+++ app/src/main/java/com/example/nolmyeon/activity/MyScrapActivity.java
@@ -1,17 +1,75 @@
 package com.example.nolmyeon.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
+import com.example.nolmyeon.GlobalApplication;
 import com.example.nolmyeon.R;
+import com.example.nolmyeon.RetrofitClient;
+import com.example.nolmyeon.adapter.MyScrapAdapter;
+import com.example.nolmyeon.adapter.ShareImageAdapter;
+import com.example.nolmyeon.model.MyData;
+import com.example.nolmyeon.model.Scrap;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MyScrapActivity extends AppCompatActivity {
-
+    RecyclerView mRecyclerView;
+    MyScrapAdapter mAdapter;
+    //뒤로가기
+    ImageButton backBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my_scrap);
 
+        setContentView(R.layout.activity_my_scrap);
+        backBtn = findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        // use a linear layout manager
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        getScrap();
+        // specify an adapter (see also next example)
+        mAdapter = new MyScrapAdapter(getApplicationContext(),GlobalApplication.getScrapArrayList());
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+    public void getScrap(){
+        RetrofitClient retrofitClient = new RetrofitClient();
+        Call<ArrayList<Scrap>> call = retrofitClient.service.getScrap(GlobalApplication.getUser_number());
+        call.enqueue(new Callback<ArrayList<Scrap>>() {
+            @Override
+            public void onResponse(Call<ArrayList<Scrap>> call, Response<ArrayList<Scrap>> response) {
+                if(response.isSuccessful() && response.body() != null){
+                    GlobalApplication.setScrapArrayList(response.body());
+                }
+            }
+            @Override
+            public void onFailure(Call<ArrayList<Scrap>> call, Throwable t) {
+                Log.d("TAG_SCRAP", t.getMessage());
+            }
+        });
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
     }
 }
--- app/src/main/java/com/example/nolmyeon/adapter/MyScrapAdapter.java
+++ app/src/main/java/com/example/nolmyeon/adapter/MyScrapAdapter.java
@@ -1,35 +1,56 @@
 package com.example.nolmyeon.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.nolmyeon.R;
+import com.example.nolmyeon.model.MyData;
+import com.example.nolmyeon.model.Scrap;
+
 import org.jetbrains.annotations.NotNull;
 
-public class MyScrapAdapter extends RecyclerView.Adapter<MyScrapAdapter.Holder> {
+import java.util.ArrayList;
 
+public class MyScrapAdapter extends RecyclerView.Adapter<MyScrapAdapter.Holder> {
+    private ArrayList<Scrap> listData = new ArrayList<>();
+    Context context;
+    public MyScrapAdapter(Context context, ArrayList<Scrap> listData) {
+        this.listData = listData;
+        this.context = context;
+    }
     @NonNull
     @NotNull
     @Override
     public Holder onCreateViewHolder(@NonNull @NotNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_my_scrap, parent, false);
+        MyScrapAdapter.Holder holder = new MyScrapAdapter.Holder(view);
+        return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull @NotNull Holder holder, int position) {
-
+        holder.tv_title.setText(listData.get(position).getTitle());
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return listData.size();
     }
 
     public class Holder extends RecyclerView.ViewHolder {
+        TextView tv_title;
+        ImageView iv_image;
         public Holder(@NonNull @NotNull View itemView) {
             super(itemView);
+            tv_title = itemView.findViewById(R.id.item_title);
+            iv_image = itemView.findViewById(R.id.item_image);
         }
     }
 }
